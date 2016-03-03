8f51fd525f1ff6900cb7b60e5737b69456076d6f
Rahul Zoldyck <rahulzoldyck@gmail.com>
2016-03-03 14:00:36 +0530

    added config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+client = MongoClient("mongodb://localhost:27017/")
+db = client['CrApp']
+gcmKey='AIzaSyDfrRm3GiVZD6ycoJ9YFRwy3Y7sMKyzmDQ'
+parseAppId = "TrAIQXwU3FAEWHGeR4QAQETp9M8FXRHlUM9FtddN"
+parseRestAPIKey = "i24KDppfH6MRoLOrfFjHkX9qMEIP0uwDofjMqEm2"
+secretKey = "CruzadoMillyhak_DeltaIsGReaT"
